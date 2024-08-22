104668dbd17ebf0b0754f19d25f67b8adeaf9209
Enio Aires <enio.aires@hotmail.com>
2024-08-22 12:28:03 -0300

    feat: initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	fmt.Println("The .env file has been loaded")
+
+	fmt.Println(os.Getenv("TESTE"))
+}
